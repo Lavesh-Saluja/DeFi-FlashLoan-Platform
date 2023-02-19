3bb17e54d6a3caa0b6593540d6e324a4b250b7a4
sm-creative-crazy4code <snehacreative2016@gmail.com>
2023-02-20 01:57:05 +0530

    adding custum code to create dex and deploy scripts for testnet

--- ContractBackend/scripts/deployFlashLoan.js
+++ ContractBackend/scripts/deployFlashLoan.js
@@ -1,21 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
   console.log("deploying...");
-  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
-  const flashLoan = await FlashLoan.deploy(
-    "0xC911B590248d127aD18546B186cC6B324e99F02c"
+  const FlashLoanArbitrage = await hre.ethers.getContractFactory(
+    "FlashLoanArbitrage"
+  );
+  const flashLoanArbitrage = await FlashLoanArbitrage.deploy(
+    "0xc4dCB5126a3AfEd129BC3668Ea19285A9f56D15D"
   );
 
-  await flashLoan.deployed();
+  await flashLoanArbitrage.deployed();
 
-  console.log("Flash loan contract deployed: ", flashLoan.address);
+  console.log("Flash loan contract deployed: ", flashLoanArbitrage.address);
 }
 
 main().catch((error) => {
