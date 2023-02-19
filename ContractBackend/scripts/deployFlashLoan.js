6e7ada5c024dc1eb198909775b6f5b2028fe89ad
@Lavesh-Saluja <b521032@iiit-nh.ac.in>
2023-02-19 22:45:38 +0530

    added Contract

--- ContractBackend/scripts/deployFlashLoan.js
+++ ContractBackend/scripts/deployFlashLoan.js
@@ -5,12 +5,19 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+
 async function main() {
-  
+  console.log("deploying...");
+  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
+  const flashLoan = await FlashLoan.deploy(
+    "0xC911B590248d127aD18546B186cC6B324e99F02c"
+  );
+
+  await flashLoan.deployed();
+
+  console.log("Flash loan contract deployed: ", flashLoan.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
