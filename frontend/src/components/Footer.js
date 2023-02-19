4c07ce1a3d256b3edaa2eea3bec6c73b6aced8f0
Aabhas <aabhasagarwal1234@gmail.com>
2023-02-19 20:42:39 +0530

    frontend updated

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer class="bg-gray-200 text-center lg:text-left">
+                <div class="text-gray-700 text-center p-4">
+                    © 2021 Copyright:
+                    <a class="text-gray-800" href="">The Four Musketeers</a>
+                </div>
+            </footer>
+        </>
+    )
+}
+export default Footer;
