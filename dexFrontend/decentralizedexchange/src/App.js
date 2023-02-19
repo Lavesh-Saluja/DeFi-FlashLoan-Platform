3253fc6cd4f017e599ae00b3865a886658d06ee6
sm-creative-crazy4code <snehacreative2016@gmail.com>
2023-02-20 05:26:15 +0530

    dex contract frontend

---
+++ dexFrontend/decentralizedexchange/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Input from './Input';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Input/>
+    </div>
+  );
+}
+
+export default App;
---
+++ dexFrontend/decentralizedexchange/src/Input.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col  w-full mt-10 md:mt-0">
+      
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">BuyDai</button>
+         
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Deposit DAI</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+     
+    
+
+
+    <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Deposit Usdc</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+     
+    
+
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Withdraw Amount</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+     
+
+
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Dai Balances</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Get Balances</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+     
+
+      <div class="relative mb-4">
+        <label for="email" class="leading-7 text-sm text-gray-600">Usdc Balance</label>
+        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+    
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Input
